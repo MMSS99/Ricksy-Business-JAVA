e9e51cd673f5bc28c432f3f58e37bcc8c939bdf1
M. Marcelo S. Salgado <mmssalgado09@gmail.com>
2025-05-04 16:54:20 +0200

    feat(CreditCard): :sparkles: añadir clase CreditCard con métodos simples

---
+++ src/main/java/edu/estatuas/ricksybusiness/CreditCard.java
@@ -0,0 +1,34 @@
+package edu.estatuas.ricksybusiness;
+
+public class CreditCard {
+    private final String owner;
+    private final String number;
+    private double credit;
+    private final String SYMBOL = "EZI";
+
+
+    CreditCard(String owner, String PAN) {
+        this.owner = owner;
+        this.number = PAN;
+        this.credit = 3000.0;
+    }
+
+    CreditCard(String owner, String PAN, double credit) {
+        this.owner = owner;
+        this.number = PAN;
+        this.credit = credit;
+    }
+
+    public String number(){
+        return number;
+    }
+
+    public double credit(){
+        return credit;
+    }
+
+    @Override
+    public String toString() {
+        return "owner: " + owner + "\nPAN: " + number + "\nCredit: " + credit + " " + SYMBOL;
+    }
+}
